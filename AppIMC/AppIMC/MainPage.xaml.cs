63c6a58bb6d785b8bcb5fb4384e2fca7e139ec4d
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2022-09-25 17:14:09 -0500

    Init

---
+++ AppIMC/AppIMC/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppIMC
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var peso = Peso.Text;
+            var altura = Altura.Text;
+            decimal imc;
+
+            if (!String.IsNullOrEmpty(peso) || !String.IsNullOrEmpty(altura))
+            {
+                imc = Decimal.Parse(peso) / (Decimal.Parse(altura)/100);
+
+                Imc.Text = Math.Round((imc),2).ToString();
+            }
+            else
+            {
+                DisplayAlert("Datos incorrectos", "Favor de validar la información ingresada", "Ok");
+            }
+
+           
+
+        }
+
+ 
+    }
+}
